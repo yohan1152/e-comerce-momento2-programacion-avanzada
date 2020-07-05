0d6cc688b1f8013e58d6080f7d5bbd499cd9e4f7
Santiago Tavera Gomez <santiago.tavera.gomez@gmail.com>
2020-07-05 15:02:54 -0500

    cambio de estilos

--- utils/mocks/products.js
+++ utils/mocks/products.js
@@ -11,42 +11,42 @@ const productMocks = [
       price: 300,
       image: "/static/images/bike.jpg"
     },
-    {
-      key: 3,
-      name: "Mountain bike",
-      price: 500,
-      image: "/static/images/Mountainbike.jpg"
-    },
-    {
-      key: 4,
-      name: "Bluetooh Spekaer",
-      price: 120,
-      image: "/static/images/BluetoohSpeaker.jpg"
-    },
-    {
-      key: 5,
-      name: "Mountain Boots",
-      price: 500,
-      image: "/static/images/MountainBoots.jpg"
-    },
-    {
-      key: 6,
-      name: "Pc Gaming",
-      price: 18000,
-      image: "/static/images/PcGaming.jpg"
-    },
-    {
-      key: 7,
-      name: "Smart Watch",
-      price: 1200,
-      image: "/static/images/SmartWatch.jpg"
-    },
-    {
-      key: 8,
-      name: "Lantern Camping",
-      price: 12,
-      image: "/static/images/LanternCamping.jpeg"
-    }
+    // {
+    //   key: 3,
+    //   name: "Mountain bike",
+    //   price: 500,
+    //   image: "/static/images/Mountainbike.jpg"
+    // },
+    // {
+    //   key: 4,
+    //   name: "Bluetooh Spekaer",
+    //   price: 120,
+    //   image: "/static/images/BluetoohSpeaker.jpg"
+    // },
+    // {
+    //   key: 5,
+    //   name: "Mountain Boots",
+    //   price: 500,
+    //   image: "/static/images/MountainBoots.jpg"
+    // },
+    // {
+    //   key: 6,
+    //   name: "Pc Gaming",
+    //   price: 18000,
+    //   image: "/static/images/PcGaming.jpg"
+    // },
+    // {
+    //   key: 7,
+    //   name: "Smart Watch",
+    //   price: 1200,
+    //   image: "/static/images/SmartWatch.jpg"
+    // },
+    // {
+    //   key: 8,
+    //   name: "Lantern Camping",
+    //   price: 12,
+    //   image: "/static/images/LanternCamping.jpeg"
+    // }
   ];
 
   module.exports = productMocks;
